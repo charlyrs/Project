ac24afeedf28b5f56cd7c8522a4f7e5727f6bc1e
Liliya <charlyrsmail@gmail.com>
2021-05-17 10:13:40 +0300

    -

--- App/App/Program.cs
+++ App/App/Program.cs
@@ -23,6 +23,10 @@ namespace App
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    
+                });
     }
 }
